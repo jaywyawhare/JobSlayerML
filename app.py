200b8d055a85bedeadee860eecfd95b421e5c347
jaywyawhare <2005441@kiit.ac.in>
2023-08-31 16:24:27 +0530

    App updated

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from sklearn.utils import all_estimators
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score, roc_auc_score, f1_score, recall_score
 from sklearn.impute import SimpleImputer
+import plotly.express as px
+import plotly.graph_objects as go
 
 submit_clicked = False
 
@@ -28,7 +30,7 @@ def perform_regression(X_train, y_train, X_test, y_test, selected_model):
     mse = mean_squared_error(y_test, y_pred)
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
-    return model, mse, mae, r2
+    return model, mse, mae, r2, y_pred
 
 def perform_classification(X_train, y_train, X_test, y_test, selected_model):
     model = selected_model()
@@ -38,10 +40,30 @@ def perform_classification(X_train, y_train, X_test, y_test, selected_model):
     roc_auc = roc_auc_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
-    return model, accuracy, roc_auc, f1, recall
+    return model, accuracy, roc_auc, f1, recall, y_pred
+
+def create_regression_plot(y_test, y_pred):
+    st.subheader("Actual vs. Predicted Scatter Plot")
+    trace_actual = go.Scatter(x=y_test, y=y_test, mode='markers', name='Actual', marker=dict(color='blue'))
+    trace_predicted = go.Scatter(x=y_test, y=y_pred, mode='markers', name='Predicted', marker=dict(color='red'))
+    
+    scatter_fig = go.Figure(data=[trace_actual, trace_predicted])
+    scatter_fig.update_layout(
+        xaxis_title='Actual Y',
+        yaxis_title='Predicted Y',
+        title='Actual vs. Predicted Values (Regression)'
+    )
+    st.plotly_chart(scatter_fig)
+
+def create_classification_heatmap(y_test, y_pred):
+    st.subheader("Classification Confusion Matrix Heatmap")
+    confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'])
+    heatmap_fig = px.imshow(confusion_matrix, color_continuous_scale='Blues', labels=dict(x="Predicted", y="Actual"))
+    st.plotly_chart(heatmap_fig)
 
 def main():
-    global submit_clicked 
+    global submit_clicked
+    global show_scatter_plot
     
     st.title("JobSlayerML : Who needs job security?")
 
@@ -77,6 +99,8 @@ def main():
     st.sidebar.header("Choose a Model")
     model_selector = st.sidebar.selectbox("Select a model:", sorted([name for name, _ in all_estimators(type_filter=['regressor' if task == 'Regression' else 'classifier'])]))
     
+    show_scatter_plot = st.sidebar.checkbox("Show Scatter Plot", value=False)
+
     if st.sidebar.button("Submit"):
         submit_clicked = True 
 
@@ -86,21 +110,22 @@ def main():
             st.header("Regression Task")
             st.write(f"Selected Model: {model_selector}")
 
-            model, mse, mae, r2 = perform_regression(X_train, y_train, X_test, y_test, selected_model)
+            model, mse, mae, r2, y_pred = perform_regression(X_train, y_train, X_test, y_test, selected_model)
             st.write(f"Mean Squared Error: {mse}")
             st.write(f"Mean Absolute Error: {mae}")
             st.write(f"R-squared (R2): {r2}")
+            create_regression_plot(y_test, y_pred)
 
         elif task == "Classification":
             st.header("Classification Task")
             st.write(f"Selected Model: {model_selector}")
 
-            model, accuracy, roc_auc, f1, recall = perform_classification(X_train, y_train, X_test, y_test, selected_model)
+            model, accuracy, roc_auc, f1, recall, y_pred = perform_classification(X_train, y_train, X_test, y_test, selected_model)
             st.write(f"Accuracy: {accuracy}")
             st.write(f"ROC AUC: {roc_auc}")
             st.write(f"F1 Score: {f1}")
             st.write(f"Recall: {recall}")
-
+            create_classification_heatmap(y_test, y_pred)
 
 if __name__ == "__main__":
     main()
