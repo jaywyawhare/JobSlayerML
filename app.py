accbf6675fef79101d174302874c991ec124161d
jaywyawhare <2005441@kiit.ac.in>
2023-10-25 02:25:07 +0530

    Modular code with feature update

--- app.py
+++ app.py
@@ -1,131 +1,186 @@
-import streamlit as st
+from src import *
+from src.models import models, eval, training
+from src.plot_generator import (
+    generate_regression_plots,
+    generate_confusion_matrix,
+    generate_heatmap,
+    generate_scatter_matrix,
+    generate_boxplot,
+    generate_histogram,
+    generate_barplot,
+    generate_countplot,
+    generate_violinplot,
+)
+from src.preprocessing import (
+    drop_columns,
+    fill_nan,
+    encode_categorical,
+    data_scaling,
+    train_test_validation_split,
+    label_encode,
+)
+
 import pandas as pd
-import numpy as np
-from sklearn.utils import all_estimators
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score, roc_auc_score, f1_score, recall_score
-from sklearn.impute import SimpleImputer
-import plotly.express as px
+import streamlit as st
+import plotly.figure_factory as ff
 import plotly.graph_objects as go
 
-submit_clicked = False
 
-def load_data():
-    uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+st.set_page_config(layout="wide")
+
+
+def main():
+    global submit_clicked
+
+    st.title("JobSlayerML : Who needs job security?")
+
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 
     if uploaded_file is not None:
         try:
             df = pd.read_csv(uploaded_file)
-            return df
+            st.sidebar.success("File uploaded successfully")
         except Exception as e:
             st.sidebar.error(f"Error: {str(e)}")
             return None
-    else:
-        return None
 
-def perform_regression(X_train, y_train, X_test, y_test, selected_model):
-    model = selected_model()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    return model, mse, mae, r2, y_pred
+        st.write(df)
+        st.write("Description of the dataset")
+        st.write(df.describe())
 
-def perform_classification(X_train, y_train, X_test, y_test, selected_model):
-    model = selected_model()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    roc_auc = roc_auc_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    return model, accuracy, roc_auc, f1, recall, y_pred
+        st.sidebar.subheader("Specify Parameters")
+        target_column = st.sidebar.selectbox("Select the target column", df.columns)
+        test_size = st.sidebar.slider("Select the test size", 0.0, 1.0, 0.2, 0.05)
+        random_state = st.sidebar.slider("Select the random state", 0, 100, 42, 1)
 
-def create_regression_plot(y_test, y_pred):
-    st.subheader("Actual vs. Predicted Scatter Plot")
-    trace_actual = go.Scatter(x=y_test, y=y_test, mode='markers', name='Actual', marker=dict(color='blue'))
-    trace_predicted = go.Scatter(x=y_test, y=y_pred, mode='markers', name='Predicted', marker=dict(color='red'))
-    
-    scatter_fig = go.Figure(data=[trace_actual, trace_predicted])
-    scatter_fig.update_layout(
-        xaxis_title='Actual Y',
-        yaxis_title='Predicted Y',
-        title='Actual vs. Predicted Values (Regression)'
-    )
-    st.plotly_chart(scatter_fig)
+        st.sidebar.subheader("Specify Model Options")
+        model_type = st.sidebar.selectbox(
+            "Select the model type", ["Regression", "Classification"]
+        )
+        comparative_analysis = st.sidebar.checkbox("Comparative Analysis")
+        if comparative_analysis:
+            model_selector = "Comparative Analysis"
+        else:
+            model_selector = st.sidebar.selectbox(
+                "Select the model", [name for name, model in models(model_type)]
+            )
 
-def create_classification_heatmap(y_test, y_pred):
-    st.subheader("Classification Confusion Matrix Heatmap")
-    confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'])
-    heatmap_fig = px.imshow(confusion_matrix, color_continuous_scale='Blues', labels=dict(x="Predicted", y="Actual"))
-    st.plotly_chart(heatmap_fig)
+        if model_type == "Regression":
+            eval_metrics = st.sidebar.multiselect(
+                "Select the evaluation metrics",
+                [
+                    "MSE",
+                    "RMSE",
+                    "MAE",
+                    "R2",
+                    "Adjusted R2",
+                    "Explained Variance",
+                    "Max Error",
+                    "Mean Absolute Percentage Error",
+                    "Median Absolute Error",
+                    "Mean Squared Log Error",
+                    "Mean Poisson Deviance",
+                    "Mean Gamma Deviance",
+                    "Mean Tweedie Deviance",
+                ],
+            )
+        else:
+            eval_metrics = st.sidebar.multiselect(
+                "Select the evaluation metrics",
+                ["Accuracy", "Precision", "Recall", "F1", "F2", "AUC", "ROC"],
+            )
+        comparison_metrics = st.sidebar.selectbox(
+            "Select the comparison metric", eval_metrics
+        )
 
-def main():
-    global submit_clicked
-    global show_scatter_plot
-    
-    st.title("JobSlayerML : Who needs job security?")
+        st.sidebar.subheader("Specify Preprocessing Options")
+        columns_to_drop = st.sidebar.multiselect(
+            "Select the columns to drop", df.columns
+        )
+        fillna_option = st.sidebar.selectbox(
+            "Select the fillna option", ["Mean", "Mode", "0"]
+        )
+        encoding_option = st.sidebar.selectbox(
+            "Select the encoding option", ["Label Encoding", "One-Hot Encoding"]
+        )
+        encoding_columns = st.sidebar.multiselect(
+            "Select the columns to encode", df.columns
+        )
+        scaling_option = st.sidebar.selectbox(
+            "Select the scaling option",
+            ["Standard Scaler", "MinMax Scaler", "Robust Scaler", "Normalizer"],
+        )
+        scaling_columns = st.sidebar.multiselect(
+            "Select the columns to scale", df.columns
+        )
+        st.sidebar.subheader("Specify Plot Options")
+        if model_type == "Regression":
+            plot_options = [
+                "Regression Plot",
+                "Scatter Matrix",
+                "Box Plot",
+                "Histogram",
+            ]
+        else:
+            plot_options = [
+                "Confusion Matrix",
+                "Heatmap",
+                "Scatter Matrix",
+                "Box Plot",
+                "Histogram",
+                "Bar Plot",
+                "Count Plot",
+                "Violin Plot",
+            ]
+        plot_selector = st.sidebar.multiselect(
+            "Select the plots to generate", plot_options
+        )
 
-    df = load_data()
+        submit_clicked = st.sidebar.button("Submit")
 
-    if df is None:
-        return
+        if not submit_clicked:
+            return None
+        else:
+            st.write("Running the app...")
 
-    st.header("Data")
-    st.write(df)
+        df = drop_columns(df, columns_to_drop)
+        df = pd.DataFrame(df)
+        df = fill_nan(df, fillna_option)
+        df = encode_categorical(df, encoding_option, encoding_columns)
+        df, scaler = data_scaling(df, scaling_option, scaling_columns)
+        X_train, X_test, y_train, y_test = train_test_validation_split(
+            df, target_column, test_size, random_state
+        )
+        y_test, y_pred, best_model = training(
+            model_type,
+            model_selector,
+            X_train,
+            y_train,
+            X_test,
+            y_test,
+            eval_metrics,
+            comparison_metrics,
+        )
 
-    st.sidebar.header("Data Preprocessing Options")
-    fillna_option = st.sidebar.radio("Fill NaN with:", ("Mean", "Mode", "0"))
+        if "Regression Plot" in plot_selector:
+            generate_regression_plots(X_train, X_test, y_train, y_test, best_model)
+        if "Confusion Matrix" in plot_selector:
+            generate_confusion_matrix(y_test, y_pred)
+        if "Heatmap" in plot_selector:
+            generate_heatmap(df)
+        if "Scatter Matrix" in plot_selector:
+            generate_scatter_matrix(df)
+        if "Box Plot" in plot_selector:
+            generate_boxplot(df)
+        if "Histogram" in plot_selector:
+            generate_histogram(df)
+        if "Bar Plot" in plot_selector:
+            generate_barplot(df)
+        if "Count Plot" in plot_selector:
+            generate_countplot(df)
+        if "Violin Plot" in plot_selector:
+            generate_violinplot(df)
 
-    task = st.sidebar.radio("Choose a Task:", ("Regression", "Classification"))
-
-    if fillna_option == "Mean":
-        fill_value = df.mean()
-    elif fillna_option == "Mode":
-        fill_value = df.mode().iloc[0]
-    else:
-        fill_value = 0
-
-    df.fillna(fill_value, inplace=True)
-
-    target_column = st.sidebar.selectbox("Select the Target Column:", df.columns)
-
-    X = df.drop(columns=[target_column])
-    y = df[target_column]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    st.sidebar.header("Choose a Model")
-    model_selector = st.sidebar.selectbox("Select a model:", sorted([name for name, _ in all_estimators(type_filter=['regressor' if task == 'Regression' else 'classifier'])]))
-    
-    show_scatter_plot = st.sidebar.checkbox("Show Scatter Plot", value=False)
-
-    if st.sidebar.button("Submit"):
-        submit_clicked = True 
-
-        selected_model = [est for name, est in all_estimators(type_filter=['regressor' if task == 'Regression' else 'classifier']) if name == model_selector][0]
-
-        if task == "Regression":
-            st.header("Regression Task")
-            st.write(f"Selected Model: {model_selector}")
-
-            model, mse, mae, r2, y_pred = perform_regression(X_train, y_train, X_test, y_test, selected_model)
-            st.write(f"Mean Squared Error: {mse}")
-            st.write(f"Mean Absolute Error: {mae}")
-            st.write(f"R-squared (R2): {r2}")
-            create_regression_plot(y_test, y_pred)
-
-        elif task == "Classification":
-            st.header("Classification Task")
-            st.write(f"Selected Model: {model_selector}")
-
-            model, accuracy, roc_auc, f1, recall, y_pred = perform_classification(X_train, y_train, X_test, y_test, selected_model)
-            st.write(f"Accuracy: {accuracy}")
-            st.write(f"ROC AUC: {roc_auc}")
-            st.write(f"F1 Score: {f1}")
-            st.write(f"Recall: {recall}")
-            create_classification_heatmap(y_test, y_pred)
 
 if __name__ == "__main__":
     main()
